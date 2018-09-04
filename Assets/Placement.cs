03ba110e8039a294453bdf592f3bdb6dbb105d9c
sean regan <rego363@gmail.com>
2018-09-04 12:43:07 +0100

    First commit

---
+++ Assets/Placement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(0.05f, 0.2f, 0.5f);
+        //transform.rotation = new quaternion(transform.rotation.w, transform.rotation.x - 90.0f, transform.;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
